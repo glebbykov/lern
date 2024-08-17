73fddec960a82996b17c916232cb51e452d877e3
glebbykov <122178014+glebbykov@users.noreply.github.com>
2024-08-17 06:01:23 +0300

    Update line_count.sh

--- bash_scripts/level4/line_couth/line_count.sh
+++ bash_scripts/level4/line_couth/line_count.sh
@@ -18,7 +18,7 @@ while IFS= read -r dir; do
     line_count=$(cat "$dir"/.*"$1"* "$dir"/*"$1"* 2>/dev/null | wc -l)
     
     # Если нужно подсчитывать файлы только на одном уровне директории
-    # line_count=$(find "$dir" -maxdepth 1 -type f \( -name ".*$1*" -o -name "*$1*" \) -exec cat {} + 2>/dev/null | wc -l)
+    # line_count=$(find "$dir" -maxdepth 1 -type f \( -name ".*$1*" -o -name "*$1*" \) -print0 | xargs -0 cat 2>/dev/null | wc -l)
 
     echo "Общее количество строк во всех файлах в директории $dir, содержащих '$1' в имени (включая скрытые файлы): $line_count"
 
